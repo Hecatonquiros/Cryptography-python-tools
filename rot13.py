98c60f0403e7b2e45ae88a95907582b0845f4f7b
Hecatonquiros <43936224+Hecatonquiros@users.noreply.github.com>
2019-08-18 20:14:21 +0200

    Rot13 algorithm

---
+++ rot13.py
@@ -0,0 +1,22 @@
+
+#Number of letters = 26
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number_letters = len(alphabet)
+string_in = raw_input("Enter message to cipher: ")
+string_out = ""
+n = len(string_in)
+
+shift = 13
+
+
+#Shifting the letters of the message 
+
+for i in range(n):
+    letter = string_in[i]
+    location = alphabet.find(letter)
+    new_location = location + shift
+    string_out += alphabet[new_location % number_letters]   #Module to allow every shift number
+
+
+print("This is the message cipher: " + string_out)
